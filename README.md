37d03f9a905455fa9aacffdc906d8e54e3f48aff
Tim Gates <tim.gates@iress.com>
2019-07-24 08:00:35 +1000

    Apply template

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Spelling - spell checker for CI
 CI configuration engine for pyspelling using plugins like resplendent and unanimous to make CI integration easier.
 
+[![Build Status](https://dev.azure.com/timgates/timgates/_apis/build/status/resplendent-dev.spelling?branchName=master)](https://dev.azure.com/timgates/timgates/_build/latest?definitionId=17&branchName=master)
+[![PyPi version](https://img.shields.io/pypi/v/spelling.svg)](https://pypi.org/project/spelling)
+[![Python Versions](https://img.shields.io/pypi/pyversions/spelling.svg)](https://pypi.org/project/spelling)
+[![PyPi downloads per month](https://img.shields.io/pypi/dm/spelling.svg)](https://pypi.org/project/spelling)
+[![Documentation Status](https://readthedocs.org/projects/spelling/badge/?version=latest)](https://spelling.readthedocs.io/en/latest/?badge=latest)
+
 # Additional Documentation:
+* [Online Documentation](https://spelling.readthedocs.io/en/latest/)
 * [News](NEWS.rst).
+* [Template Updates](COOKIECUTTER_UPDATES.md).
 * [Code of Conduct](CODE_OF_CONDUCT.md).
 * [Contribution Guidelines](CONTRIBUTING.md).
--- ci/in_docker/test.sh
+++ ci/in_docker/test.sh
@@ -5,12 +5,14 @@ set -euxo pipefail
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR="$( dirname "$( dirname "${THISDIR}" )" )"
 
-source ${BASEDIR}/ci/in_docker/prepare.sh
+# shellcheck source=/dev/null
+source "${BASEDIR}/ci/in_docker/prepare.sh"
 
+cd "${BASEDIR}"
+find . -iname \*.sh -print0 | xargs -0 shellcheck
 # Version independant checks
 PYVER=3.7
 # Run pyspelling in root to check docs
-cd "${BASEDIR}"
 "python${PYVER}" -m pyspelling
 cd "${BASEDIR}/app"
 # Version dependant checks
@@ -19,6 +21,8 @@ for PYVER in ${PYTHONVERS} ; do
   "python${PYVER}" -m isort -rc -c --diff "${MODULES[@]}"
   "python${PYVER}" -m bandit -r "${MODULES[@]}"
   find "${MODULES[@]}" -iname \*.py -print0 | xargs -0 -n 1 "${BASEDIR}/ci/in_docker/pylint.sh" "python${PYVER}"
-  "python${PYVER}" -m pytest -n auto --cov-config=.coveragerc --cov-fail-under=100 "--cov=${MAIN_MODULE}" --cov-report=xml:test-cov.xml --cov-report=html
+  "python${PYVER}" -m pytest -n auto --cov-config=.coveragerc --cov-fail-under=40 "--cov=${MAIN_MODULE}" --cov-report=xml:test-cov.xml --cov-report=html
 done
+# validate doco
+"${BASEDIR}/ci/in_docker/doco.sh"
 echo 'Testing Complete'
