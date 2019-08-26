7da36970186fe2d57429a3f2d85698bfe5caff6a
Tim Gates <tim.gates@iress.com>
2019-08-26 17:22:11 +1000

    Begin adding support for easier execution of spell checker

--- app/spelling/__main__.py
+++ app/spelling/__main__.py
@@ -1,21 +1,62 @@
+#!/usr/bin/env python
 """
 Module load handler for execution via:
 
 python -m spelling
 """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import click
+import pkg_resources
+import pyspelling.__main__
+
+from .version import __version__
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
-def main():
+@click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
+@click.version_option(version=__version__)
+@click.pass_context
+def main(ctxt):
     """
-    Test
+    Used to conveniently invoke spell checking with sensible defaults and
+    command line arguments to change the behavior.
     """
-    print("""\
-For more information view the online documentation at:
-https://spelling-dev.readthedocs.io/en/latest/
-""")
+    if ctxt.invoked_subcommand is None:
+        run_invocation()
 
 
-if __name__ == '__main__':
-    main()
+@main.command()
+def invoke():
+    """
+    Invoke the spell checker
+    """
+    run_invocation()
+
+
+def run_invocation():
+    """
+    Execute the invocation
+    """
+    configpath = pkg_resources.resource_filename(__name__, '.pyspelling.yml')
+    fail = pyspelling.__main__.run(
+        configpath,
+        names=[],
+        groups=[],
+        binary='',
+        spellchecker='',
+        sources=[],
+        verbose=0,
+        debug=False,
+    )
+
+    if fail:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
+# vim: set ft=python:
