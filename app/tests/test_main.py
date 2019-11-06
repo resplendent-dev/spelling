f2b172e604e6f3f0088a74a93aa7e5e18c2e3166
Tim Gates <tim.gates@iress.com>
2019-11-06 18:56:19 +1100

    Increase coverage

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,14 +3,17 @@ Test modules for spelling.__main__
 """
 
 
+import pytest
 from click.testing import CliRunner
-
 from spelling.__main__ import main
 
 from .util import get_tmpdir
 
 
-def test_main_emptydir():
+@pytest.mark.parametrize(
+    "args,", [(), ("--display-context",), ("--no-display-context",)]
+)
+def test_main_emptydir(args):
     """
     GIVEN an empty directory WHEN calling main THEN the call executes
     successfully with a exit_code of 0 and a success message
@@ -19,7 +22,7 @@ def test_main_emptydir():
     runner = CliRunner()
     with get_tmpdir():
         # Exercise
-        result = runner.invoke(main, [])
+        result = runner.invoke(main, args)
     # Verify
     assert result.output.strip() == "Spelling check passed :)"  # nosec
     assert result.exit_code == 0  # nosec
