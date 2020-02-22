d67be3887610aff33a113bcb4671f4c4c6c1006d
Tim Gates <tim.gates@iress.com>
2020-02-23 07:25:50 +1100

    Fix missing param

--- app/spelling/check.py
+++ app/spelling/check.py
@@ -33,10 +33,10 @@ def check_iter(display_context, display_summary, config, storage_path, workingpa
     Execute the invocation
     """
     all_results = run_spell_check(config, storage_path, workingpath)
-    yield from process_results(all_results)
+    yield from process_results(all_results, display_context, display_summary)
 
 
-def process_results(all_results):
+def process_results(all_results, display_context, display_summary):
     """
     Work through the results yielding the misspelt words in a human readable
     output.
