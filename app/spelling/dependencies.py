ab03c2ab3ff69abe637f7d82acbdaa91f60bc124
Tim Gates <tim.gates@iress.com>
2020-04-22 07:41:18 +1000

    Dependency installer ;

--- app/spelling/dependencies.py
+++ app/spelling/dependencies.py
@@ -2,16 +2,89 @@
 Make sure aspell is installed.
 """
 
+import subprocess  # noqa=S404
+import sys
+
 import distro
 from whichcraft import which
+
+HANDLERS = {
+    "debian": [
+        ["sudo", "--non-interactive", "apt-get", "update"],
+        [
+            "sudo",
+            "--non-interactive",
+            "apt-get",
+            "install",
+            "-y",
+            "aspell",
+            "aspell-en",
+        ],
+    ],
+    "arch": [
+        [
+            "sudo",
+            "--non-interactive",
+            "pacman",
+            "-S",
+            "aspell",
+            "aspell-en",
+        ],
+    ],
+    "rhel": [
+        [
+            "sudo",
+            "--non-interactive",
+            "yum",
+            "install",
+            "epel-release",
+        ],
+        [
+            "sudo",
+            "--non-interactive",
+            "yum",
+            "install",
+            "aspell",
+            "aspell-en",
+        ],
+    ],
+    "fedora": [
+        [
+            "sudo",
+            "--non-interactive",
+            "dnf",
+            "install",
+            "aspell",
+            "aspell-en",
+        ],
+    ]
+}
 
 
 def ensure_dependencies():
     """
     Check for presence of aspell
     """
-    print(distro.id())
-    print(which("aspell"))
+    if which("aspell") is not None:
+        return
+    distro_id = distro.id()
+    distro_like = distro.like()
+    try:
+        try:
+            handler = HANDLERS[distro_id]
+        except KeyError:
+            handler = HANDLERS[distro_like]
+    except KeyError:
+        print(
+            f"Unsupported Distribution: {distro_id} please add me to "
+            f"https://github.com/resplendent-dev/spelling/blob/master"
+            f"/app/spelling/dependencies.py",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    else:
+        for cmdargs in handler:
+            subprocess.call(cmdargs)  # noqa=S603
 
 
 if __name__ == "__main__":
