b91c554e014d5e91bdc776db900f6c236751f313
Tim Gates <tim.gates@iress.com>
2020-04-24 09:44:58 +1000

    display version

--- travis.sh
+++ travis.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Validate working from pypi
+python3 --version
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
 sudo python3 -m pip install nox
