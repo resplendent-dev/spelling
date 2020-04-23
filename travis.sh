bdf988d44c4619bf3a2b9877577b1b75351679da
Tim Gates <tim.gates@iress.com>
2020-04-24 08:26:31 +1000

    fix install

--- travis.sh
+++ travis.sh
@@ -5,7 +5,9 @@ set -euxo pipefail
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Validate working from pypi
-python3 -m pip install nox
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+sudo python3 -m pip install nox
 nox
 
 # Validate regular CI
